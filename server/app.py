f264f104856dcdd4c89a3c344eb5b3a499827668
Kyle Greenidge <kylegreen149@gmail.com>
2024-09-21 16:09:44 -0400

    Done

--- server/app.py
+++ server/app.py
@@ -21,6 +21,44 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def show_earthquake(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        body = {
+            "id": earthquake.id,
+            "magnitude": earthquake.magnitude,
+            "location": earthquake.location,
+            "year": earthquake.year
+            }
+        return make_response(body, 200)
+    else:
+        body = {"message": f'Earthquake {id} not found.'}
+        return make_response(body, 404)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def show_magnitude(magnitude):
+    magnitude = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    count = len(magnitude)
+    if magnitude:
+        body = {
+            "count": count,
+            "quakes": [
+                {
+                    "id": mag.id,
+                    "location": mag.location,
+                    "magnitude": mag.magnitude,
+                    "year": mag.year
+                } for mag in magnitude
+            ]
+        }
+        return make_response(body, 200)
+    else:
+        body = {
+            "count": 0,
+            "quakes": []
+        }
+        return make_response(body, 200)
 
 
 if __name__ == '__main__':
